d8e8ef51bbc4d72b1999c010b1b9f422ed38c1df
Felipe Duarte Silva <felipeds0211@gmail.com>
2025-08-04 17:09:31 -0300

    add ajustes

--- script.js
+++ script.js
@@ -40,3 +40,39 @@ const swiper = new Swiper('.wrapper', {
     },
 
 });
+
+
+  const wrapper = document.getElementById("carousel-wrapper");
+  const list = document.getElementById("carousel-list");
+
+  let scrollAmount = 0;
+  let scrollStep = 0;
+
+  function updateScrollStep() {
+    const larguraProduto = wrapper.clientWidth;
+    if (window.innerWidth < 768) {
+      scrollStep = larguraProduto / 1;
+    } else if (window.innerWidth < 1024) {
+      scrollStep = larguraProduto / 2;
+    } else {
+      scrollStep = larguraProduto / 3;
+    }
+  }
+
+  updateScrollStep();
+  window.addEventListener("resize", updateScrollStep);
+
+  function autoScroll() {
+    scrollAmount += scrollStep;
+
+    if (scrollAmount >= list.scrollWidth - wrapper.clientWidth) {
+      scrollAmount = 0;
+    }
+
+    wrapper.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth"
+    });
+  }
+
+  setInterval(autoScroll, 6000);
